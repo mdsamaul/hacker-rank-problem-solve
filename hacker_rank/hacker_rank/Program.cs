e1399d1735b83b8725f2db227e5c63ff80f81382
mdsamaul <mdsamaul843@gmail.com>
2024-08-27 11:20:17 +0600

    d arry abs

---
+++ hacker_rank/hacker_rank/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hacker_rank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://www.hackerrank.com/challenges/diagonal-difference/problem?isFullScreen=true
+            //1 2 3
+            //4 5 6
+            //9 8 9
+            //output |15-17|=2
+            int n;
+            Console.Write("enter the  number : ");
+            n = int.Parse(Console.ReadLine());
+            int[,] arr = new int[n,n];
+            for (int i = 0; i < n; i++)
+            {
+               for(int j = 0; j < n; j++)
+                {
+                    Console.Write($"{i},{j} : ");
+                    arr[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            dSum(arr,n);
+            Console.Read();
+        }
+        public static int dSum(int[,] ar, int n)
+        {
+            int res1 = 0;
+            int res2 = 0;
+            int result = 0;
+            for (int i = 0; i < n; i++) {
+               for ( int j=0; j < n; j++)
+                {
+                    if (i == j) {
+                        //Console.Write($"{i} , {j} = {ar[i,j]}");
+                        res1 += ar[i, j];
+                    }
+                     if (i+j == n-1 ) {
+                     //Console.Write($"{i} , {j} = {ar[i, j]} \n");
+                     res2+= ar[i, j]; 
+                    }
+                }
+
+               //Console.WriteLine();
+            }
+            //Console.WriteLine($"{res1},{res2}");
+
+            result=Math.Abs(res1-res2);
+            return result;
+        }
+    }
+
+    
+}
